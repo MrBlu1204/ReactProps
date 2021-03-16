b398007bca9374cf6c39458d45534f96c977647b
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-16 16:15:50 +0530

    Adding more components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Cards from "./Cards";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQGfdb4vzF-ZYg/profile-displayphoto-shrink_200_200/0/1608130799123?e=1616025600&v=beta&t=PwGkSnnpaJDgP7LFScIG_mU5fvlgy06Tc-P8v4CTl4w" />
       <Cards
         name={contacts[0].name}
         img={contacts[0].imgURL}

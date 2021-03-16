31782fb9c1aa46a91e5fe0f83395a5100128aa4d
Manish Ranjan Behera <manish12042000@gmail.com>
2021-03-16 17:13:38 +0530

    Mapping Components {list_name.map(mapping_fuction)};
    key = {unique value/ index}

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,25 @@ import Cards from "./Cards";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCards(contact) {
+  return (
+    <Cards
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQGfdb4vzF-ZYg/profile-displayphoto-shrink_200_200/0/1608130799123?e=1616025600&v=beta&t=PwGkSnnpaJDgP7LFScIG_mU5fvlgy06Tc-P8v4CTl4w" />
-      <Cards
+      {contacts.map(createCards)};
+      {/* <Cards
         name={contacts[0].name}
         img={contacts[0].imgURL}
         phone={contacts[0].phone}
@@ -25,7 +38,7 @@ function App() {
         img={contacts[2].imgURL}
         phone={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
